1ebab670c3858d520fa3805c254c0452bf1c46e0
Durjaya Das <153779228+Dur-jaya@users.noreply.github.com>
2025-02-21 23:09:03 +0530

    Time: 21 ms (72.24%), Space: 47.3 MB (67.12%) - LeetHub

---
+++ 1387-find-elements-in-a-contaminated-binary-tree/1387-find-elements-in-a-contaminated-binary-tree.java
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class FindElements {
+  public FindElements(TreeNode root) {
+    dfs(root, 0);
+  }
+
+  public boolean find(int target) {
+    return vals.contains(target);
+  }
+
+  private Set<Integer> vals = new HashSet<>();
+
+  private void dfs(TreeNode root, int val) {
+    if (root == null)
+      return;
+
+    root.val = val;
+    vals.add(val);
+    dfs(root.left, val * 2 + 1);
+    dfs(root.right, val * 2 + 2);
+  }
+}
